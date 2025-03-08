c9db8646a79074a542ee5eb9580fa6ca731b46a2
Corey Megown <corey.megown@gmail.com>
2025-03-07 23:36:18 -0800

    chore: add some basic styles

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,20 @@
+import { DateTime } from 'luxon';
+import { IdAttributePlugin } from '@11ty/eleventy';
 import EleventyVitePlugin from '@11ty/eleventy-plugin-vite';
 
 export default async function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('src/assets');
 
+  eleventyConfig.addPlugin(IdAttributePlugin);
   eleventyConfig.addPlugin(EleventyVitePlugin);
+
+  eleventyConfig.addFilter('humanReadable', (date) => {
+    return DateTime.fromJSDate(date, { zone: 'utc'}).toLocaleString(DateTime.DATE_FULL);
+  });
+
+  eleventyConfig.addFilter('machineReadable', (date) => {
+    return DateTime.fromJSDate(date, { zone: 'utc' }).toISODate();
+  });
 };
 
 export const config = {
